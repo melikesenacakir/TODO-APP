4258118eb40175b45e3ef690cc39fea7f5202b01
melikesenacakir <melike.cakir2001@gmail.com>
2023-09-23 16:55:04 +0300

    düzenlemeler

--- register.php
+++ register.php
@@ -41,6 +41,10 @@
                     <div class="d-grid gap-3">
                         <input type="submit" class="btn btn-outline-primary mt-2" name="buton" value="kayıt ol">
                     </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z"/>
+                    </svg>
+                    <a href="login.php" class="text-decoration-none link-dark ">Login Sayfasına Geri Dön</a>
                 </div>
             </form>
         </div>
